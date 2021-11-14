b97fc900d8580f51851455d0d99f9de92b0a3bf6
yftang5 <yftang5@gmail.com>
2021-11-14 01:36:24 -0500

    Add patient entity

--- src/main/java/com/uhn/patientmanagementservice/PatientManagementServiceApplication.java
+++ src/main/java/com/uhn/patientmanagementservice/PatientManagementServiceApplication.java
@@ -8,6 +8,7 @@ public class PatientManagementServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PatientManagementServiceApplication.class, args);
+		System.out.println("Sevice is running");
 	}
 
 }
