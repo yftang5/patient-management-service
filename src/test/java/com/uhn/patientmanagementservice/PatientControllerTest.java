4c344d33c768c4e74c1514daf27a713d8c08654b
yftang5 <yftang5@gmail.com>
2021-11-14 01:25:52 -0500

    Add unit test case for patient controller

---
+++ src/test/java/com/uhn/patientmanagementservice/PatientControllerTest.java
@@ -0,0 +1,37 @@
+package com.uhn.patientmanagementservice;
+
+import org.junit.jupiter.api.Test;
+
+public class PatientControllerTest {
+
+	@Test
+	public void getAllPatients_success() throws Exception{
+		
+	}
+	
+	@Test
+	public void retrievePatientById_success() throws Exception{
+
+	}
+	
+	@Test
+	public void retrievePatientById_notFound() throws Exception{
+
+	}
+	
+	@Test
+	public void retrievePatientByFirstName_success() throws Exception{
+
+	}
+	
+	@Test
+	public void retrievePatientByFirstName_notFound() throws Exception{
+
+	}
+	
+	@Test
+	public void createPatient_success() throws Exception{
+		
+	}
+	
+}
