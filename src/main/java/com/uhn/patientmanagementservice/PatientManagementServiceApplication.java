e0718b3358871ab008cbf3dd6995cf0f3d794279
yftang5 <yftang5@gmail.com>
2021-11-14 00:47:40 -0500

    Add spring boot project

---
+++ src/main/java/com/uhn/patientmanagementservice/PatientManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uhn.patientmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uhn/patientmanagementservice/PatientManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uhn.patientmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
